baeaf19099bda7a8c9a58e4fab2455dee744aa84
Fabio Valim <oficialvalim@gmail.com>
2025-05-03 18:07:19 -0300

    bug .env

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
     dialect:  process.env.DIALECT,
     host: process.env.HOST,
-    username: process.env.USERNAME,
+    username: process.env.DB_USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.DB_PORT,

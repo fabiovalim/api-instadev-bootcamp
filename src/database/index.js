c9fe2b506f55bbc8bb1347e1b5b912aca5f6c6c2
Fabio Valim <oficialvalim@gmail.com>
2025-05-04 19:04:15 -0300

    create Models

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
+const Users = require('../apps/models/Users');
 const databaseConfig = require('../configs/db');
+
+const models = [Users];
 
 class Database {
     constructor() {
@@ -7,6 +10,8 @@ class Database {
     };
     init() {
         this.connection = new Sequelize(databaseConfig);    
+
+        models.map((model) => model.init(this.connection));
     };
 };
 

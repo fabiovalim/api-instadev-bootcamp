744dff394080d7821320b39e657cb16ed4629e74
Fabio Valim <oficialvalim@gmail.com>
2025-05-05 09:39:28 -0300

    add userController: create user

---
+++ src/apps/controllers/UserController.js
@@ -0,0 +1,20 @@
+const Users = require('../models/Users');
+
+class UserController {
+    async create(req, res) {
+        const verifyUser = await Users.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if(verifyUser) {
+            return res.status(400).json({message: 'User already exists!'});
+        };
+
+        const user = await Users.create(req.body);
+        res.send({ user });
+    };
+};
+
+module.exports = new UserController();

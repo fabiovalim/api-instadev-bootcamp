cb4a44de9296164d65b751e07333223f74dba27b
Fabio Valim <oficialvalim@gmail.com>
2025-05-07 10:03:37 -0300

    delete user

--- src/routes.js
+++ src/routes.js
@@ -9,15 +9,13 @@ const UserSchema = require('./schema/create.user.schema.json');
 const routes = new Router();
 
 routes.post('/user', schemaValidator(UserSchema), UserController.create);
-
 routes.post('/auth', schemaValidator(AuthSchema), AuthenticationController.authenticate);
-
 routes.get('/health', (req, res) => {
     return res.send({message: `Connected with success!`});
 });
 
 routes.use(AuthenticationMiddleware);
-
 routes.put('/user', UserController.update);
+routes.delete('/user', UserController.delete);
 
 module.exports = routes;

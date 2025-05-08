5e37a15af94acfcd3b220005fc51f996deaf8546
Fabio Valim <oficialvalim@gmail.com>
2025-05-08 08:29:08 -0300

    add multer to files upload

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,22 @@
 const { Router } = require('express');
+const { upload } = require('./configs/multer');
 const schemaValidator = require('./apps/middlewares/schemaValidator')
 const AuthenticationMiddleware = require('./apps/middlewares/authentication');
 const AuthenticationController = require('./apps/controllers/AuthenticationController');
 const AuthSchema = require('./schema/auth.schema.json');
 const UserController = require('./apps/controllers/UserController');
 const UserSchema = require('./schema/create.user.schema.json');
+const FileController = require('./apps/controllers/FileController');
 
 const routes = new Router();
 
 routes.post('/user', schemaValidator(UserSchema), UserController.create);
 routes.post('/auth', schemaValidator(AuthSchema), AuthenticationController.authenticate);
 routes.get('/health', (req, res) => {return res.send({message: `Connected with success!`})});
-
 routes.use(AuthenticationMiddleware);
 routes.put('/user', UserController.update);
 routes.delete('/user', UserController.delete);
 routes.get('/user-profile', UserController.userProfile);
+routes.post('/upload', upload.single('image'), FileController.upload);
 
 module.exports = routes;
